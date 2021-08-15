27e7a49544bf621a9fce34755cd7c0fc488063f1
peterlavrov <dotaplay4fun@gmail.com>
2021-08-15 17:45:25 +0300

    стилизация секции works + доработки

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
-// const header = document.querySelector('.header');
-// let width = document.querySelector('.home__container').offsetWidth;
-// header.style.width = width + 'px';
+// одинаковая ширина и высота у слайдов
+
+const slide = document.querySelector('.gallery-slides__slide');
+const slides = document.querySelectorAll('.gallery-slides__slide');
+let slide_sizes = slide.offsetWidth + 'px';
+
+function countSlideSizes (width) {
+  slides.forEach(el => {
+    el.style.height = width;
+  });
+}
+
+countSlideSizes(slide_sizes);
+
+window.addEventListener('resize', () => {
+  slide_sizes = slide.offsetWidth + 'px';
+  countSlideSizes(slide_sizes);
+});

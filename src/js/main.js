e46fc9d631e48b943ea7d554ded7a9ec7a7c4dbc
peterlavrov <dotaplay4fun@gmail.com>
2021-08-15 22:04:14 +0300

    стилизация секции prices

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
-// одинаковая ширина и высота у слайдов
+// одинаковая ширина и высота у слайдов и ширины углов у цен
 
 const slide = document.querySelector('.gallery-slides__slide');
 const slides = document.querySelectorAll('.gallery-slides__slide');
+
+console.log(slides);
+
 let slide_sizes = slide.offsetWidth + 'px';
 
 function countSlideSizes (width) {
@@ -10,9 +13,12 @@ function countSlideSizes (width) {
   });
 }
 
+
 countSlideSizes(slide_sizes);
 
 window.addEventListener('resize', () => {
   slide_sizes = slide.offsetWidth + 'px';
   countSlideSizes(slide_sizes);
 });
+
+
